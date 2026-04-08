3c84eaa177d4c58794e078ee65e274e22ae90527
AlexanderG8 <mpalexanderg@gmail.com>
2026-04-07 23:08:16 -0500

    Create file calculadora

---
+++ calculadora.ts
@@ -0,0 +1,7 @@
+const number1 : number = 20;
+const number2 : number = 5;
+const multiplicar = (num1 : number, num2 : number) => {
+    return num1 * num2;
+}
+
+console.log(multiplicar(number1, number2));
